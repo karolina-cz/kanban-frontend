dbcf4c1499fa27113446667760d47d51eeed06d3
Karolina Czachorska <karolina.czachorska@boombit.com>
2022-11-07 09:35:50 +0100

    add clarity tracking code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,20 +16,34 @@ export class HomeComponent implements OnInit {
   }
 
   onCreateKanbanBoard(): void {
-    // todo add hotjar
+    // add hotjar
     // tslint:disable-next-line:only-arrow-functions
-    (function(h,o,t,j,a,r){
-      // tslint:disable-next-line:only-arrow-functions
-      // @ts-ignore
-      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-      // @ts-ignore
-      h._hjSettings={hjid:3230629,hjsv:6};
-      a=o.getElementsByTagName('head')[0];
-      r=o.createElement('script');r.async=1;
-      // @ts-ignore
-      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    // (function(h,o,t,j,a,r){
+    //   // tslint:disable-next-line:only-arrow-functions
+    //   // @ts-ignore
+    //   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    //   // @ts-ignore
+    //   h._hjSettings={hjid:3230629,hjsv:6};
+    //   a=o.getElementsByTagName('head')[0];
+    //   r=o.createElement('script');r.async=1;
+    //   // @ts-ignore
+    //   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    //   a.appendChild(r);
+    // })(window, document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+    // add clarity
+    // tslint:disable-next-line:only-arrow-functions
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+
+      const head=l.getElementsByTagName('head')[0];
+
+      t=l.createElement(r);t.async=1;
+      t.src="https://www.clarity.ms/tag/"+i;
+      head.appendChild(t);
+    })(window, document, "clarity", "script", "ef4rv2hxqo");
+
+
     this.roomService.createRoom(RoomType.KANBAN_BOARD).subscribe((uuid) => {
       this.router.navigate(['/kanban-board/' + uuid]);
     });
